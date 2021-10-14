bd8b0054ef0e69a6362ad123b27370ce20e21452
ronentgreat <ronenh53@gmail.com>
2021-10-14 20:02:05 +0000

    added a docstring

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,7 @@
 import random
 
 def welcome():
+    "This functon welcomes the user and prompts him/her to enter his name  But makes sure he/she enters just letters and not numbers."
     while True:
         print("Welcome to Hangman Game!")
         print("Hi, please enter your name.")
@@ -13,6 +14,7 @@ def welcome():
             print(f"""Hi {name}. You will play against a computer.\nThe computer will randomly choose a word and you will have to try to guess the word.\nGood Luck!""" )
             break
 
+#
 
 
 welcome()
