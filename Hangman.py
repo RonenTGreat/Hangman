f3e4d08aa7ed5ee1970295a428bc695d42c11156
ronentgreat <ronenh53@gmail.com>
2021-10-14 19:46:52 +0000

    created a welcome function that welcomes the user and gets his/her name.

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,18 @@
-print("Welcome to Hangman. Enter 's' to start")
+import random
 
-start = input(">> ")
+def welcome():
+    while True:
+        print("Welcome to Hangman Game!")
+        print("Hi, please enter your name.")
+        name = input(">>").capitalize()
+        # Makes sure the user only enters letters
+        if not name.isalpha():
+            print("Numbers are not suppose to be in your name.")
+            continue
+        else:
+            print(f"""Hi {name}. You will play against a computer.\nThe computer will randomly choose a word and you will have to try to guess the word.\nGood Luck!""" )
+            break
 
-if start.lower() == 's':
-    print("I am hello")
+
+
+welcome()
